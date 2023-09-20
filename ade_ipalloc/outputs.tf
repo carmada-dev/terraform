77442f973135b38fbb54b472c2f96901dace894c
Markus Heiliger <markus.heiliger@dash-two.de>
2023-09-20 19:01:15 +0200

    ...

--- ade_ipalloc/outputs.tf
+++ ade_ipalloc/outputs.tf
@@ -1,3 +1,12 @@
 output "ip_ranges" {
+
   value = split(",", data.external.IPAlloction.result.IPRANGES)
+  description = "The allocated IP ranges by this module"
+  
+  depends_on = [ 
+    # the allocation needs to finished first
+    # otherwise the the result will be NULL
+    null_resource.IPAlloc 
+  ]
+
 }
