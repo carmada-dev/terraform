e5fce1fb1e7e2af22d60fea45976a9c9d551e11c
Markus Heiliger <markus.heiliger@dash-two.de>
2023-09-21 21:26:07 +0200

    ...

--- ade_environment/main.tf
+++ ade_environment/main.tf
@@ -3,30 +3,30 @@ data "external" "Environment" {
 }
 
 data "azurerm_resource_group" "Environment" {
-  name = "${data.external.Environment.ENVIRONMENT_RESOURCE_GROUP_NAME}"
+  	name = "${data.external.Environment.ENVIRONMENT_RESOURCE_GROUP_NAME}"
 }
 
 resource "arm2tf_unique_string" "Environment" {
-  input = [ data.azurerm_resource_group.Environment.id ]
+  	input = [ data.azurerm_resource_group.Environment.id ]
 }
 
 data "azurerm_app_configuration_key" "Settings_PrivateLinkDnsZoneRG" {
-  configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
-  key                    = "PrivateLinkDnsZoneRG"
+  	configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
+  	key                    = "PrivateLinkDnsZoneRG"
 }
 
 data "azurerm_app_configuration_key" "Settings_ProjectNetworkId" {
-  configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
-  key                    = "ProjectNetworkId"
+	configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
+	key                    = "ProjectNetworkId"
 }
 
 data "azurerm_app_configuration_key" "Settings_ProjectGatewayIP" {
-  configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
-  key                    = "ProjectGatewayIP"
+	configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
+	key                    = "ProjectGatewayIP"
 }
 
 data "azurerm_app_configuration_key" "Settings_IPAlloc_URL" {
-  configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
-  key                    = "IPAlloc-URL"
+	configuration_store_id = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationStoreId"]
+	key                    = "IPAlloc-URL"
 }
 
