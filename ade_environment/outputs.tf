cccbaeef3988a19d381d117ca108083beaea49ee
Markus Heiliger <markus.heiliger@dash-two.de>
2023-09-21 21:45:09 +0200

    ...

--- ade_environment/outputs.tf
+++ ade_environment/outputs.tf
@@ -1,54 +1,54 @@
 output "EnvironmentName" {
 
-  value = data.azurerm_resource_group.Environment.name
+  value       = data.azurerm_resource_group.Environment.name
   description = "Get the environment name"
 
 }
 
 output "EnvironmentLocation" {
 
-  value = data.azurerm_resource_group.Environment.location
+  value       = data.azurerm_resource_group.Environment.location
   description = "Get the environment location"
 
 }
 
 output "EnvironmentSuffix" {
 
-  value = arm2tf_unique_string.Environment.result
+  value       = arm2tf_unique_string.Environment.result
   description = "Get the environment suffix"
 
 }
 
 output "EnvironmentType" {
   
-  value = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationLabel"]
+  value       = data.azurerm_resource_group.Environment.tags["hidden-ConfigurationLabel"]
   description = "Get the environment type"
 }
 
 output "PrivateLinkDnsZoneRG" {
 
-  value = azurerm_app_configuration_key.PrivateLinkDnsZoneRG.value
+  value       = data.azurerm_app_configuration_key.PrivateLinkDnsZoneRG.value
   description = "Get setting PrivateLinkDnsZoneRG"
 
 }
 
 output "ProjectNetworkId" {
 
-  value = azurerm_app_configuration_key.ProjectNetworkId.value
+  value       = data.azurerm_app_configuration_key.ProjectNetworkId.value
   description = "Get setting ProjectNetworkId"
 
 }
 
 output "ProjectGatewayIP" {
 
-  value = azurerm_app_configuration_key.ProjectGatewayIP.value
+  value       = data.azurerm_app_configuration_key.ProjectGatewayIP.value
   description = "Get setting ProjectGatewayIP"
 
 }
 
 output "IPAlloc_URL" {
 
-  value = azurerm_app_configuration_key.IPAlloc_URL.value
+  value       = data.azurerm_app_configuration_key.IPAlloc_URL.value
   description = "Get setting IPAlloc_URL"
 
 }
