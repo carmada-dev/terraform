5879d2bd300de53b832cb1add50ab89e04338d90
Markus Heiliger <markus.heiliger@dash-two.de>
2023-09-21 10:58:11 +0200

    ...

--- ade_ipalloc/main.tf
+++ ade_ipalloc/main.tf
@@ -9,13 +9,14 @@ resource "null_resource" "IPAlloc" {
 	triggers = {
 		AllocationUrl = "${trimsuffix(trimspace(data.azurerm_app_configuration_key.Settings_IPAlloc_URL.value), "/")}/allocation/${uuid()}"
 		AllocationQuery = "env=${trimspace(var.configurationLabel)}&cidr=${join("&cidr=", var.cidrBlocks)}"
+		VerboseSwitch = "${var.verbose ? "-v" : "-sS"}"
 	}
 
 	provisioner "local-exec" {
     	command = <<-EOC
 
 TOKEN=$(az account get-access-token --query accessToken --output tsv)
-curl ${var.verbose ? "-v" : "-sS"} -X POST "${self.triggers.AllocationUrl}?${self.triggers.AllocationQuery}" -H "Accept: application/json" -H "Authorization: Bearer $TOKEN"
+curl ${self.VerboseSwitch} -X POST "${self.triggers.AllocationUrl}?${self.triggers.AllocationQuery}" -H "Accept: application/json" -H "Authorization: Bearer $TOKEN"
 
 EOC
   	}
@@ -25,7 +26,7 @@ EOC
 		command = <<-EOC
 
 TOKEN=$(az account get-access-token --query accessToken --output tsv)
-curl ${var.verbose ? "-v" : "-sS"} -X DELETE "${self.triggers.AllocationUrl}" -H "Accept: application/json" -H "Authorization: Bearer $TOKEN"
+curl ${self.VerboseSwitch} -X DELETE "${self.triggers.AllocationUrl}" -H "Accept: application/json" -H "Authorization: Bearer $TOKEN"
 
 EOC
 	}
