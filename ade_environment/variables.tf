164e73d8734375917d4ef0775510b702645f9d69
Markus Heiliger <markus.heiliger@dash-two.de>
2023-09-22 11:45:25 +0200

    ...

--- ade_environment/variables.tf
+++ ade_environment/variables.tf
@@ -1,5 +1,6 @@
 variable "resourceGroup" {
 	type 		= string
 	nullable	= true
+	default 	= null
 	description = "Name of the environment resource group"
 }
--- ade_ipalloc/variables.tf
+++ ade_ipalloc/variables.tf
@@ -1,16 +1,19 @@
 variable "resourceGroup" {
 	type 		= string
 	nullable	= true
+	default 	= null
 	description = "Name of the environment resource group"
 }
 
 variable "cidrBlocks" {
 	type 		= list(number)
 	nullable 	= false
+	description = "The list of CIDR block to allocate"
 }
 
 variable "verbose" {
 	type		= bool
 	nullable	= false
 	default 	= false
+	description = "Execute module in verbose mode"
 }
